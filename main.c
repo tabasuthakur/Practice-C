35fc428c244464d766ea0540f89bbedcbf601ba6
taniabt <>
2021-06-14 15:51:45 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int i = 0;
+  int j = 0;
+  int k = i+j;
+  printf("%d\n", k);
+  printf("Hello World\n");
+  int a =0;
+   for( a = 10; a < 20; a = a + 1 ){
+      printf("value of a: %d\n", a);
+   }
+  return 0;
+}
